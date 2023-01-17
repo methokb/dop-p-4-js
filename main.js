224683a43364b7446a003624bd65a59a377f806a
methokb <abdurasulovrahmatso@gmail.com>
2023-01-17 20:32:01 +0300

    dop p 4 js

---
+++ main.js
@@ -0,0 +1,17 @@
+// function minMax(arr){
+//     return [Math.max(...arr), Math.min(...arr)]
+// }
+// console.log(minMax([1,2,3,4,5]));
+
+
+// function minCity(arr){
+//     return[Math.max(...arr), Math.min(...arr)]
+// }
+// console.log(minCity([5,4,-3,2,1]));
+
+
+
+function min(arr){
+    return[Math.max(...arr), Math.min(...arr)]
+}
+console.log(min([1, 9, 3, 4, -5]));
